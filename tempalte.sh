39c228a8b88e46c7721ac6f3b6763a72d0684279
Amin Zouai <aminzouai7@gmail.com>
2025-07-20 00:07:41 +0100

    folder structure added

---
+++ tempalte.sh
@@ -0,0 +1,13 @@
+#crating  directory mkdir 
+mkdir -p src  
+mkdir -p reseach 
+#creating files 
+touch src/_init__.py 
+touch src/helper.py 
+touch README.md 
+touch .env 
+touch setup.py 
+touch requirements.txt 
+touch reseach/trials.ipynb 
+touch app.py 
+echo "directory and file  created successfully."
